a6897a92a82ff56b976ee7411d542a4ca3804da4
kodzib <kbrowq@gmail.com>
2025-05-02 14:45:29 +0200

    example raylib i readme update

--- Projekt_objektowka.cpp
+++ Projekt_objektowka.cpp
@@ -1,20 +1,91 @@
-// Projekt_objektowka.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿/*******************************************************************************************
+*
+*   raylib [core] example - Mouse input
+*
+*   Example complexity rating: [★☆☆☆] 1/4
+*
+*   Example originally created with raylib 1.0, last time updated with raylib 5.5
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2014-2025 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
 
-#include <iostream>
+#include "raylib.h"
 
-int main()
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - mouse input");
+
+    Vector2 ballPosition = { -100.0f, -100.0f };
+    Color ballColor = DARKBLUE;
+    int isCursorHidden = 0;
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //---------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyPressed(KEY_H))
+        {
+            if (isCursorHidden == 0)
+            {
+                HideCursor();
+                isCursorHidden = 1;
+            }
+            else
+            {
+                ShowCursor();
+                isCursorHidden = 0;
+            }
+        }
+
+        ballPosition = GetMousePosition();
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) ballColor = MAROON;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_SIDE)) ballColor = PURPLE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ballColor = BEIGE;
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawCircleV(ballPosition, 40, ballColor);
+
+        DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, DARKGRAY);
+        DrawText("Press 'H' to toggle cursor visibility", 10, 30, 20, DARKGRAY);
+
+        if (isCursorHidden == 1) DrawText("CURSOR HIDDEN", 20, 60, 20, RED);
+        else DrawText("CURSOR VISIBLE", 20, 60, 20, LIME);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Projekt_objektowka
+
+[Jak zainstalować](https://youtu.be/UiZGTIYld1M?si=uzkW5xooa4Cia0Fn)
